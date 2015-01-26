8ac8d2a97a5825c51129f9104b61fe069e1805ef
rodu <greenheron35@gmail.com>
2015-01-26 17:05:09 +0000

    Fixes returning data when we ask to load a single url

--- qunit-fixtures-loader.js
+++ qunit-fixtures-loader.js
@@ -1,22 +1,24 @@
 /* global QUnit: true */
 $(function fixturesLoader(){
   'use strict';
-  
+
   QUnit.extend(QUnit, {
-    
+
     loadFixtures: function loadFixtures(urls){
         var deferred = $.Deferred();
         // When all requests will complete...
         $.when.apply(this, $.map(urls, function mapUrls(url){
           return $.ajax(url);
-        
+
         })).done(function doneFn(){ // once all $.ajax complete
           // Resolves the promise returning the HTML for fixtures in order
           deferred.resolve.apply(this,
             $.map(Array.prototype.slice.apply(arguments), function mapArgs(arg){
-              return arg[0];
-          // arguments of the doneFn is an array of data returned by each $.ajax
-          // for each url
+              // The arg parameter received is an array of data items created
+              // with the results returned by each $.ajax for each url. That
+              // will sit at the position 0 of the arg array. If we requested a
+              // single url, arg will be the result retrieved for that url.
+              return $.isArray(arg) ? arg[0] : arg;
           }));
 
         }).fail(deferred.reject);
